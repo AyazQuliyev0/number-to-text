fcaadf2d024df114eb356fdf97d1e96ee6b7e724
Ayaz Quliyev <ayazaraz950@gmail.com>
2024-11-11 19:40:10 +0400

    updated

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
 function numberToWord(number) {
-    if (number<0 || number > 100) {
-        console.log("none")
+    if (number < 0 || number > 100) {
+        return "none";
     }
-    const units = [
-        "sıfır", "bir", "iki", "üç", "dörd", 
-        "beş", "altı", "yeddi", "səkkiz", "doqquz"
-    ];
-
-    const tens = [
-        "", "on", "iyirmi", "otuz", "qırx", 
-        "əlli", "altmış", "yetmiş", "səksən", "doxsan"
-    ];
-
-    if (number === 100) {
-        return "yüz";
+    switch (number) {
+        case 100:
+            return "yüz";
+        case 0:
+            return "sıfır";
+    }
+    const tens = Math.floor(number / 10);
+    const units = number % 10;
+    let result = "";
+    switch (tens) {
+        case 1:
+            result += "on"; 
+            break;
+        case 2:
+            result += "iyirmi"; 
+            break;
+        case 3: 
+            result += "otuz"; 
+            break;
+        case 4: 
+            result += "qırx"; 
+            break;
+        case 5: 
+            result += "əlli"; 
+            break;
+        case 6: 
+            result += "altmış"; 
+            break;
+        case 7: 
+            result += "yetmiş"; 
+            break;
+        case 8: 
+            result += "səksən"; 
+            break;
+        case 9: 
+            result += "doxsan"; 
+            break;
     }
 
-    const tensPart = Math.floor(number / 10);
-    const unitsPart = number % 10;
-
-    if (tensPart === 0) {
-        return units[unitsPart];
-    } else if (unitsPart === 0) {
-        return tens[tensPart];
-    } else {
-        return tens[tensPart] + " " + units[unitsPart];
+    if (units !== 0) {
+        if (result) result += " ";
+        switch (units) {
+            case 1: result += "bir"; break;
+            case 2: result += "iki"; break;
+            case 3: result += "üç"; break;
+            case 4: result += "dörd"; break;
+            case 5: result += "beş"; break;
+            case 6: result += "altı"; break;
+            case 7: result += "yeddi"; break;
+            case 8: result += "səkkiz"; break;
+            case 9: result += "doqquz"; break;
+        }
     }
+
+    return result;
 }
 
 const input = parseInt(prompt(""));
